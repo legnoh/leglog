386a8b74dc1cf65a9a1986ceb6bb4a33d8f1e84a
Ryoma Kai <legknmj@icloud.com>
2020-05-17 12:00:50 +0900

    fix week number logic

--- .github/workflows/mkdir-post.sh
+++ .github/workflows/mkdir-post.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 set -ex
 
-# set env in local 
-year=$(date +%Y)
-month=$(date +%m)
-week=$(date +%U)
-date=$(date +%d)
-slug=${year}-${week}
-from_date=$(date '+%Y-%m-%d')
-to_date=$(date '+%Y-%m-%d' --date '6 days')
-post_dir=content/posts/$(expr $(date +%Y) / 10)x/${year}/${week}
+if [[ "$(uname)" == 'Darwin' ]]; then
+    shopt -s expand_aliases
+    alias date='gdate'
+fi
 
-# set env in global
+sunday='sunday'
+if [ $(date +%w) -ne 0 ] ; then
+  sunday="next sunday"
+fi
+
+year=$(date +%Y --date "${sunday}")
+week=$(date +%U --date "${sunday}")
+slug=${year}-${week}
+to_date=$(date '+%Y-%m-%d' --date "${sunday}")
+from_date=$(date '+%Y-%m-%d' --date "6 day ago ${to_date}")
+post_dir=content/posts/$(expr ${year} / 10)x/${year}/${week}
+
+# set env in global(GitHub Actions)
 echo "::set-env name=slug::${slug}"
+echo "::set-env name=from_date::${from_date}"
+echo "::set-env name=to_date::${to_date}"
 echo "::set-env name=post_dir::${post_dir}"
 
 # mkdir, hero alias
