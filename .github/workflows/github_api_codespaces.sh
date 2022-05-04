a3ae3424b4c80b886ec98983bd29101adb1a22ab
Ryoma Kai <legknmj@icloud.com>
2022-05-04 11:37:27 +0900

    [skip ci] create codespaces functions

---
+++ .github/workflows/github_api_codespaces.sh
@@ -0,0 +1,41 @@
+create_codespace_for_pr() {
+    local token=${1:?}
+    local owner=${2:?}
+    local repo=${3:?}
+    local pr_number=${4:?}
+    local location=${5:?}
+
+    echo "Creating Codespace: ${owner}/${repo}/${pr_number}@${ws}"
+
+    curl -f -X "POST" "https://api.github.com/repos/${owner}/${repo}/pulls/${pr_number}/codespaces" \
+            -H 'Accept: application/vnd.github.v3+json' \
+            -H "Authorization: Bearer ${token}" \
+            -H 'Content-Type: application/json; charset=utf-8' \
+            -d $'{"location": "SouthEastAsia"}'
+}
+
+get_pullreq_unused_spaces() {
+    local token=${1:?}
+    local owner=${2:?}
+    local repo=${3:?}
+    local ref=${4:?}
+    target_workspaces=$(curl -f -X "GET" "https://api.github.com/repos/${owner}/${repo}/codespaces" \
+            -H 'Accept: application/vnd.github.v3+json' \
+            -H "Authorization: Bearer ${token}" \
+            -H 'Content-Type: application/json; charset=utf-8' \
+            | jq -r ".codespaces[] | select(.git_status.ref == \"${ref}\") | select (.git_status.has_unpushed_changes == false) | select (.git_status.has_uncommitted_changes == false) | .name")
+    echo ${target_workspaces}
+}
+
+delete_codespace() {
+    local token=${1:?}
+    local owner=${2:?}
+    local name=${3:?}
+
+    echo "Deleting Codespace: ${owner}/${repo}: ${ws}"
+    
+    curl -f -X "DELETE" "https://api.github.com/repos/${owner}/codespaces/${name}" \
+            -H 'Accept: application/vnd.github.v3+json' \
+            -H "Authorization: Bearer ${token}" \
+            -H 'Content-Type: application/json; charset=utf-8'
+}
