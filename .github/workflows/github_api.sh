44a60ee62ec0280e09ea37e746c4db3562f15238
Ryoma Kai <legknmj@icloud.com>
2022-05-04 12:18:29 +0900

    [skip ci] debug

--- .github/workflows/github_api.sh
+++ .github/workflows/github_api.sh
@@ -1,3 +1,15 @@
+get_branch_name_by_pr_number(){
+    local token=${1:?}
+    local owner=${2:?}
+    local repo=${3:?}
+    local pr_number=${4:?}
+    curl -f -X "GET" "https://api.github.com/repos/${owner}/${repo}/pulls/${pr_number}" \
+            -H 'Accept: application/vnd.github.v3+json' \
+            -H "Authorization: Bearer ${token}" \
+            -H 'Content-Type: application/json; charset=utf-8' \
+            | jq -r ".head.ref")
+}
+
 create_codespace_for_pr() {
     local token=${1:?}
     local owner=${2:?}
